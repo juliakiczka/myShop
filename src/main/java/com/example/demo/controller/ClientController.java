5e2cef7c50194a5b88e2e261ccbda7d579ff36e1
juliakiczka <juliakiczka838@gmail.com>
2023-06-13 21:06:32 +0200

    adding controller with all client's methods

--- src/main/java/com/example/demo/controller/ClientController.java
+++ src/main/java/com/example/demo/controller/ClientController.java
@@ -28,15 +28,6 @@ public class ClientController {
         return service.getAllClients();
     }
 
-    @PostMapping("")
-    public ResponseEntity<Client> postClient(@RequestBody Client requestClient) {
-        Client savedClient = service.saveClient(requestClient);
-        log.info("{} saved", savedClient);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(savedClient);
-    }
-
     @GetMapping("/{id}")
     public ResponseEntity<Client> getClientById(@PathVariable Long id) {
         log.info("getting client with id {}", id);
@@ -44,5 +35,32 @@ public class ClientController {
         return response
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteExample(@PathVariable Long id) {
+        if (service.getById(id).isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            service.removeById(id);
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> postClient(@RequestBody Client requestClient) {
+        Client savedClient = service.saveClient(requestClient);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(savedClient);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client client ) {
+        final Client updatedClient = service.updateById(id, client);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(updatedClient);
+
     }
 }
--- src/main/java/com/example/demo/service/ClientService.java
+++ src/main/java/com/example/demo/service/ClientService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.Client;
 import com.example.demo.repository.JpaClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,12 +20,25 @@ public class ClientService {
     }
 
     public Client saveClient(Client client) {
-        repository.save(client);
-        return client;
+        return repository.save(client);
     }
 
     public Optional<Client> getById(Long id) {
         return repository.findById(id);
     }
 
+    public void removeById(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Client updateById(Long id, Client client) {
+        Optional<Client> optionalClient = repository.findById(id);
+        if (optionalClient.isPresent()) {
+            Client existingClient = optionalClient.get();
+            client.setId(id);
+            return repository.save(client);
+        }
+        return null;
+
+    }
 }
